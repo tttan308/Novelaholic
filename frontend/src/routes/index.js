4a860f070e198f65a0dab9d522a22d5443a08b3d
ndnhattan <tan.ndn@teko.vn>
2024-05-27 21:22:26 +0700

    Add genre selectbox and genre page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,13 +2,14 @@ import Home from '../pages/Home';
 import BookInfo from '../pages/BookInfo';
 import BookContent from '../pages/BookContent';
 import SearchPage from '../pages/SearchPage';
-import { Component } from 'react';
+import GenrePage from '../pages/GenrePage';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/book-info', component: BookInfo },
   { path: '/book-content', component: BookContent },
   { path: '/search', component: SearchPage },
+  { path: '/category/:genre', component: GenrePage },
 ];
 
 export default routes;
--- frontend/src/services/novel.js
+++ frontend/src/services/novel.js
@@ -29,3 +29,20 @@ export const searchNovels = async (keyword, page) => {
     console.error(error);
   }
 };
+
+export const searchByGenre = async (genre, page) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3001/novels/genres/${genre}?page=${page}`
+    );
+    const data = await response.json();
+    console.log(data);
+    return {
+      data: data.novels,
+      total: data.totalPages,
+      page: data.currentPage,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
