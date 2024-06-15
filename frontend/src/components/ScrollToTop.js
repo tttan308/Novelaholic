5123a885c68d072cf78046ef11ab18ed5edd24a9
21120581 <thuytruc011258@gmail.com>
2024-06-15 17:17:25 +0700

    Update scroll to top when change route

--- frontend/src/components/ScrollToTop.js
+++ frontend/src/components/ScrollToTop.js
@@ -1,11 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  const prevPathname = useRef(pathname);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const handlePopState = () => {
+      prevPathname.current = window.location.pathname;
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (prevPathname.current !== pathname) {
+      window.scrollTo(0, 0);
+    }
+    prevPathname.current = pathname;
   }, [pathname]);
 
   return null;
